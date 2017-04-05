6501934e28334b60ddb372f2bf017a5021967480
raffaelespazzoli <raffaele.spazzoli@gmail.com>
2017-04-05 13:14:00 -0400

    added json to property conversion

--- examples/spring-legacy-example/src/main/java/com/raffa/InitConfiguration.java
+++ examples/spring-legacy-example/src/main/java/com/raffa/InitConfiguration.java
@@ -6,44 +6,60 @@ import java.util.HashMap;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.AutoConfigureBefore;
-import org.springframework.context.annotation.Bean;
+import org.springframework.cloud.bootstrap.config.PropertySourceLocator;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
-import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.Environment;
 import org.springframework.core.env.MapPropertySource;
-import org.springframework.core.env.MutablePropertySources;
+import org.springframework.core.env.PropertySource;
 
-import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Configuration
-@AutoConfigureBefore({ SpringLegacyExampleApplication.class })
+//@AutoConfigureBefore({ SpringLegacyExampleApplication.class })
 @Lazy(false)
-public class InitConfiguration {
+public class InitConfiguration implements PropertySourceLocator {
 
 	private Log log = LogFactory.getLog(InitConfiguration.class);
 
 	@Value("${secret.file:/var/run/secrets/vaultproject.io/secret.json}")
 	String secretFile;
 
-	@Autowired
-	private ConfigurableEnvironment env;
-
-	@Bean
-	 public MapPropertySource secretPropertySource(String vaultToken) throws JsonParseException, JsonMappingException, IOException {
-	    ObjectMapper mapper = new ObjectMapper(); 
-	    File from = new File(secretFile); 
-	    TypeReference<HashMap<String,Object>> typeRef = new TypeReference<HashMap<String,Object>>() {};
-	    HashMap<String,Object> o = mapper.readValue(from, typeRef); 
-	    MapPropertySource secretPropertySource= new MapPropertySource("secretPropertySource", o);
-	    MutablePropertySources sources = env.getPropertySources();
-	    sources.addFirst(secretPropertySource );
-	    log.debug("added secretPropertySource: "+ o);
-	    return secretPropertySource;
-	 }
+//	@Autowired
+//	private ConfigurableEnvironment env;
+//
+//	 public MapPropertySource secretPropertySource() throws JsonParseException, JsonMappingException, IOException {
+//	    ObjectMapper mapper = new ObjectMapper(); 
+//	    File from = new File(secretFile); 
+//	    TypeReference<HashMap<String,Object>> typeRef = new TypeReference<HashMap<String,Object>>() {};
+//	    HashMap<String,Object> o = mapper.readValue(from, typeRef); 
+//	    MapPropertySource secretPropertySource= new MapPropertySource("secretPropertySource", o);
+//	    MutablePropertySources sources = env.getPropertySources();
+//	    sources.addFirst(secretPropertySource );
+//	    log.debug("added secretPropertySource: "+ o);
+//	    return secretPropertySource;
+//	 }
+	 
+	    @Override
+	    public PropertySource<?> locate(Environment environment) {
+	    	ObjectMapper mapper = new ObjectMapper(); 
+		    File from = new File(secretFile); 
+		    TypeReference<HashMap<String,Object>> typeRef = new TypeReference<HashMap<String,Object>>() {};
+		    HashMap<String, Object> o=null;
+			try {
+				o = mapper.readValue(from, typeRef);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				log.error(e);
+			}
+	        return new MapPropertySource("secretPropertySource", o);
+	    }	 
+	
+//	@Override
+//	public void afterPropertiesSet() throws Exception {
+//		log.debug("After Properties Set");
+//		secretPropertySource();
+//	}
 }
