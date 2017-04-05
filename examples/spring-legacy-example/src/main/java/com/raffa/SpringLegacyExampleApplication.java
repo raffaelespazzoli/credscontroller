e6ec282bdb8e4f131d1571ff9cbb711922cb8236
raffaelespazzoli <raffaele.spazzoli@gmail.com>
2017-04-05 13:54:24 -0400

    added actuator

--- examples/spring-legacy-example/src/main/java/com/raffa/SpringLegacyExampleApplication.java
+++ examples/spring-legacy-example/src/main/java/com/raffa/SpringLegacyExampleApplication.java
@@ -3,13 +3,11 @@ package com.raffa;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-//@PropertySource("file://${secret.file}")
 public class SpringLegacyExampleApplication {
 
 	public static void main(String[] args) {
