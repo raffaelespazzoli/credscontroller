23889d9e6e9e5371c158f81e6c26086b8f172103
raffaelespazzoli <raffaele.spazzoli@gmail.com>
2017-11-23 12:34:24 -0500

    fixes

--- examples/spring-native-example/src/main/java/com/raffa/VaultDemoApplication.java
+++ examples/spring-native-example/src/main/java/com/raffa/VaultDemoApplication.java
@@ -7,9 +7,11 @@ import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.vault.annotation.VaultPropertySource;
 
 
 @SpringBootApplication
+@VaultPropertySource("secret/spring-example")
 public class VaultDemoApplication {
 	
 	private Log log = LogFactory.getLog(VaultDemoApplication.class);
